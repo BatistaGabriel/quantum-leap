b216af54e32b603ae32f6e0b01dabdcfefc7665d
Gabriel Batista <gabriel_batista@outlook.com>
2024-12-07 16:53:51 -0300

    fix: fix `create` endpoint to handle `multipart/form-data` requests

--- src/main/java/com/quantum_leap/api/controller/EventController.java
+++ src/main/java/com/quantum_leap/api/controller/EventController.java
@@ -6,6 +6,7 @@ import com.quantum_leap.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/event")
@@ -13,9 +14,22 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-    @PostMapping
-    public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
-        Event event = this.eventService.createEvent(body);
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Event> create(
+            @RequestParam("title") String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam("date") Long date,
+            @RequestParam("city") String city,
+            @RequestParam("uf") String uf,
+            @RequestParam("remote") Boolean remote,
+            @RequestParam("eventUrl") String eventUrl,
+            @RequestParam(value = "image",  required = false)MultipartFile image
+            ){
+        EventRequestDTO data = new EventRequestDTO(
+                title, description, date, city,
+                uf, remote, eventUrl, image
+        );
+        Event event = this.eventService.createEvent(data);
 
         return ResponseEntity.ok(event);
     }
