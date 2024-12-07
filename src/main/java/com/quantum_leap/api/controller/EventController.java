ffe886923d5893dc22758ebdd0ab81d6f925ff72
Gabriel Batista <gabriel_batista@outlook.com>
2024-12-07 00:19:34 -0300

    fix: fix versioning issues

--- src/main/java/com/quantum_leap/api/controller/EventController.java
+++ src/main/java/com/quantum_leap/api/controller/EventController.java
@@ -5,10 +5,7 @@ import com.quantum_leap.api.domain.event.EventRequestDTO;
 import com.quantum_leap.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/event")
@@ -16,7 +13,7 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
         Event event = this.eventService.createEvent(body);
 
