671be703900f75bf2b223ee6f09b10264eccc9dd
Gabriel Batista <gabriel_batista@outlook.com>
2024-12-19 21:42:38 -0300

    fix: using cli local creds to auth S3 Client builder

--- src/main/java/com/quantum_leap/api/config/AWSConfig.java
+++ src/main/java/com/quantum_leap/api/config/AWSConfig.java
@@ -1,5 +1,6 @@
 package com.quantum_leap.api.config;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,6 +28,7 @@ public class AWSConfig {
     public AmazonS3 createS3Instance(){
         return AmazonS3ClientBuilder
                 .standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(awsRegion)
                 .build();
     }
