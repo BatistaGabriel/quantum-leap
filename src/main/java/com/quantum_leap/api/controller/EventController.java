979670a69c60c37057ac0f570b79993e143be3e6
Gabriel Batista <gabriel_batista@outlook.com>
2024-12-20 22:51:24 -0300

    feat: add `getUpcomingEvents` service

--- src/main/java/com/quantum_leap/api/controller/EventController.java
+++ src/main/java/com/quantum_leap/api/controller/EventController.java
@@ -39,7 +39,7 @@ public class EventController {
 
     @GetMapping
     public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
-        List<EventResponseDTO> events = this.eventService.getEvents(page, size);
+        List<EventResponseDTO> events = this.eventService.getUpcomingEvents(page, size);
 
         return ResponseEntity.ok(events);
     }
